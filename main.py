9316a5ac667d4a248e20fc2afae38e70f95477ef
CaptainCow35 <jnewman@g.pateros.org>
2023-05-15 14:58:41 -0700

    added yes

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import pygame
 import sys
 import random 
 
+
+
 WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+win_rect = WIN.get_rect()
 
-def shrink_image(image, colorkey = None):
-    new_image = pygame.transform.scale(image, (image.get_width() * .1, image.get_height() * .1))
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = new_image.get_at((0,0))
-            new_image.set_colorkey(colorkey, pygame.RLEACCEL)
-    return new_image
+clock = pygame.time.Clock()
 
-player_image = pygame.image.load("art/goodgoku.png")
-player_image = shrink_image(player_image)
-player_image.set_colorkey(player_image.get_at((0, 0)))
-player_rect = player_image.get_rect()
+def draw_block(x, y, color = (54, 92, 227)):
+    pygame.draw.rect(WIN, color, ( x, y, 15, 15), 1, 1)
+
+snake = [(300, 300), (320, 300), (340, 300)]
+
+apple_possible_x = list(range(0, win_rect.right, 20))
+random.shuffle(apple_possible_x)
+
+apple_possible_y = list(range(0, win_rect.bottom, 20))
+random.shuffle(apple_possible_y)
+
+apple = (apple_possible_x[0], apple_possible_y[0])
+
+velocity = [0, 0]
 
 while True:
 
@@ -25,29 +32,35 @@ while True:
             if event.key == pygame.K_q:
                 pygame.quit()
 
-            if event.key == pygame.K_a:
-                player_rect.x -= 50
-            if event.key == pygame.K_d:
-                player_rect.x += 50
-            if event.key == pygame.K_w:
-                player_rect.y -= 50
-            if event.key == pygame.K_s:
-                player_rect.y += 50 
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                print('left stop')
-            if event.key == pygame.K_RIGHT:
-                print('right stop')
-
-    ############### Logic below #################
-    border_rect = border_rect
-    if player_rect.colliderect(border_rect): 
-        pass
-
+            if event.key == pygame.K_a and velocity[0] > -20:
+                velocity[0] = -20
+                velocity[1] = 0
+            if event.key == pygame.K_d and velocity[0] < 20:
+                velocity[0] = 20
+                velocity[1] = 0
+            if event.key == pygame.K_w and velocity[1] > -20:
+                velocity[1] = -20
+                velocity[0] = 0
+            if event.key == pygame.K_s and velocity[1] < 20:
+                velocity[1] = 20
+                velocity[0] = 0
 
     
 
+
+    ############### Logic below #################
+    ate_apple = False
+    for snake_block in snake:
+        if snake_block == apple:
+            snake.append((snake[-1]))
+            apple = (10000, 10000)
+            ate_apple = True
+    if ate_apple != True:
+        snake.pop(-1)
+
+    snake.insert(0, (snake[0][0] + velocity[0], snake[0][1] + velocity[1]))
+    
+    
 
 
 
@@ -55,7 +68,10 @@ while True:
 
     #put things that get drawn on screen here
     WIN.fill("black")
-    WIN.blit(player_image, player_rect)
+    for snake_block in snake:
+        draw_block(snake_block[0], snake_block[1])
+
+    draw_block(apple[0], apple[1], (235, 14, 14))
 
     pygame.display.update()
-
+    clock.tick(10)
